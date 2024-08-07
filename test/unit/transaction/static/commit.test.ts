bb55f6e7c53e2303fa0c65331b92a07671997e0c
apttap <pittman.austin@gmail.com>
2024-08-07 07:38:11 -0600

    unit tests: Database.commitTransaction > Transaction.commit

--- test/unit/transaction/static/commit.test.ts
+++ test/unit/transaction/static/commit.test.ts
@@ -1,6 +1,5 @@
 // Lib
 import { Routes } from '@/database/routes.js';
-import { Database } from '@/database/Database.js';
 
 // Testing
 import {
@@ -9,15 +8,16 @@ import {
   headersWithTransaction as headers,
   variables
 } from '@test/variables.js';
+import { Transaction } from '@/transaction/Transaction.js';
 
 const endpoint = `${variables.address}${variables.apiRoute}${Routes.COMMIT}/${variables.databaseName}`;
 
-describe('Database.commitTransaction()', () => {
+describe('Transaction.commit()', () => {
   it(`should make a properly formatted POST request to ${endpoint}`, async () => {
     jest.spyOn(global, 'fetch').mockResolvedValueOnce({
       status: 204
     } as Response);
-    await Database.commitTransaction(SESSION, TRX);
+    await Transaction.commit(SESSION, TRX);
 
     // TODO: I don't like using a null body, look into this
     const requestInit: RequestInit = {
@@ -32,23 +32,19 @@ describe('Database.commitTransaction()', () => {
   it('should error if no transaction is provided', async () => {
     // Act & Assert
     // @ts-expect-error - an instance of SpriteTransaction is required
-    await expect(Database.commitTransaction(SESSION)).rejects.toMatchSnapshot();
+    await expect(Transaction.commit(SESSION)).rejects.toMatchSnapshot();
   });
   it('should error for a non-204 response', async () => {
     jest.spyOn(global, 'fetch').mockResolvedValueOnce({
       status: 205
     } as Response);
-    await expect(
-      Database.commitTransaction(SESSION, TRX)
-    ).rejects.toMatchSnapshot();
+    await expect(Transaction.commit(SESSION, TRX)).rejects.toMatchSnapshot();
   });
   it('should forward errors from internal methods', async () => {
     jest
       .spyOn(global, 'fetch')
       .mockRejectedValue(new Error('The fetch failed.'));
 
-    await expect(
-      Database.commitTransaction(SESSION, TRX)
-    ).rejects.toMatchSnapshot();
+    await expect(Transaction.commit(SESSION, TRX)).rejects.toMatchSnapshot();
   });
 });
