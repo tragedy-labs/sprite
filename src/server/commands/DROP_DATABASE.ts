0e17ca6a9880441979f5722d389193c464f017b8
Austin Pittman <pittman.austin@gmail.com>
2025-05-21 07:10:11 -0600

    fix: 'DROP DATABASE' to 'drop database'

--- src/server/commands/DROP_DATABASE.ts
+++ src/server/commands/DROP_DATABASE.ts
@@ -1,2 +1,2 @@
 export const DROP_DATABASE = (databaseName: string) =>
-  `DROP DATABASE ${databaseName}`;
+  `drop database ${databaseName}`;
--- test/unit/server/commands/dropDatabase.test.ts
+++ test/unit/server/commands/dropDatabase.test.ts
@@ -4,7 +4,7 @@ import { variables } from '@test/variables.js';
 describe('DROP_DATABASE', () => {
   it('given a databaseName (string) as an argument should return a string that is the command to drop that database', async () => {
     // Arrange
-    const expected = `DROP DATABASE ${variables.databaseName}`;
+    const expected = `drop database ${variables.databaseName}`;
     // Act
     const result = DROP_DATABASE(variables.databaseName);
     // Assert
