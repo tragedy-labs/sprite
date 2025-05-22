7a18dae7d3982f0dcec23b83041db790ba5e681e
Austin Pittman <pittman.austin@gmail.com>
2025-05-22 09:57:40 -0600

    feature: skeleton for Socket.ts

---
+++ src/socket/Socket.ts
@@ -0,0 +1,9 @@
+import { ISpriteDatabaseNewSession } from '@/session/DatabaseSession.js';
+
+class Socket {
+  constructor(params: ISpriteDatabaseNewSession) {}
+  subscribe() {}
+  unsubscribe() {}
+}
+
+export { Socket };
