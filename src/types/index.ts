3edd83acecd38737c088f5d549a307619cce6c74
Austin Pittman <pittman.austin@gmail.com>
2025-09-03 11:37:17 -0600

    refactor: update exports for context types reorg

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,6 @@
+export * from '../context/index.js';
 export * from './commands.js';
-export * from './context.js';
 export * from './database.js';
 export * from './operators.js';
 export * from './queries.js';
 export * from './type.js';
-
