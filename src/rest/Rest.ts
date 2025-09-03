c5538eca8717c0103f5c5d3b43a1c5635bef8437
Austin Pittman <pittman.austin@gmail.com>
2025-09-03 08:44:16 -0600

    refactor: rest using new context

--- src/rest/Rest.ts
+++ src/rest/Rest.ts
@@ -1,17 +1,19 @@
-import { SpriteTransaction } from '../transaction/SpriteTransaction.js';
-import { HttpClient } from './HttpClient.js';
-import { JsonResponse } from './JsonResponse.js';
-import { ArcadeHeaders } from './ArcadeHeaders.js';
-import { SpriteBody, SpriteRestBody } from './SpriteBody.js';
-import { Routes as DatabaseRoutes } from '../database/routes.js';
-import { Routes as ServerRoutes } from '../server/routes.js';
-import { Session } from '../session/Session.js';
+import { ADMIN_ROUTES } from '@/admin/constants/routes.js';
+import { ArcadeClientContext } from '@/context/types.js';
+import { DATABASE_ROUTES } from '@/database/constants/routes.js';
+import { Transaction } from '@/database/transaction/index.js';
+import { ArcadeHeaders } from '@/rest/ArcadeHeaders.js';
+import { HttpClient } from '@/rest/HttpClient.js';
+import { JsonResponse } from '@/rest/JsonResponse.js';
+import { SpriteBody, SpriteRestBody } from '@/rest/SpriteBody.js';
 
-type ApiRoutes = DatabaseRoutes | ServerRoutes | string;
+type AdminRoute = (typeof ADMIN_ROUTES)[keyof typeof ADMIN_ROUTES];
+type DatabaseRoute = (typeof DATABASE_ROUTES)[keyof typeof DATABASE_ROUTES];
+type ArcadeRoute = AdminRoute | DatabaseRoute;
 
-enum Method {
-  POST = 'POST',
-  GET = 'GET'
+const enum Method {
+  GET = 'GET',
+  POST = 'POST'
 }
 
 /**
@@ -20,69 +22,69 @@ enum Method {
 class Rest {
   /**
    * Make a {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/GET `GET`} request.
-   * @param route The route (key) to target in the request.
-   * @param session The `Session` to target in the request.
+   * @param route - The route (key) to target in the request.
+   * @param context.- The `ArcadeClientContext` to target in the request.
    * @returns A promise that resolves to a {@link https://developer.mozilla.org/en-US/docs/Web/API/Response `Response`} object.
    */
   public static async get(
-    route: ApiRoutes,
-    session: Session
+    route: ArcadeRoute,
+    context: ArcadeClientContext
   ): Promise<Response> {
-    return HttpClient.request(session.endpoints[route], {
+    return HttpClient.request(context.endpoints[route], {
       method: Method.GET,
-      headers: ArcadeHeaders.compose(session),
+      headers: ArcadeHeaders.compose(context.server.headers),
       keepalive: true
     });
   }
   /**
    * Make a {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/GET `GET`} request in which a JSON response is expected.
-   * @param route The route (key) to target in the request.
-   * @param session The `Session` to target in the request.
+   * @param route - The route (key) to target in the request.
+   * @param context.- The `ArcadeClientContext` to target in the request.
    * @returns The json response from the ArcadeDB Server.
    */
   public static async getJson<T>(
-    route: ApiRoutes,
-    session: Session
+    route: ArcadeRoute,
+    context: ArcadeClientContext
   ): Promise<T> {
-    return JsonResponse.parse<T>(await this.get(route, session));
+    return JsonResponse.parse<T>(await this.get(route, context));
   }
   /**
    * Make a {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST `POST`} request.
-   * @param session The `Session` to target in the request.
-   * @param endpoint The relative endpoint of request.
-   * @param body The {@link https://developer.mozilla.org/en-US/docs/Web/API/Request/body body} to include in the request.
-   * @param headers The {@link https://developer.mozilla.org/en-US/docs/Web/API/Request/headers headers} to include in the request.
+   * @param context.- The `ArcadeClientContext` to target in the request.
+   * @param endpoint - The relative endpoint of request.
+   * @param body - The {@link https://developer.mozilla.org/en-US/docs/Web/API/Request/body body} to include in the request.
+   * @param headers - The {@link https://developer.mozilla.org/en-US/docs/Web/API/Request/headers headers} to include in the request.
    * @returns A promise that resolves to a {@link https://developer.mozilla.org/en-US/docs/Web/API/Response `Response`} object.
    */
   public static async post(
-    route: ApiRoutes,
+    route: ArcadeRoute,
     body: SpriteRestBody,
-    session: Session,
-    transaction?: SpriteTransaction
+    context: ArcadeClientContext,
+    transaction?: Transaction
   ): Promise<Response> {
-    return HttpClient.request(session.endpoints[route], {
+    return HttpClient.request(context.endpoints[route], {
       method: Method.POST,
-      headers: ArcadeHeaders.compose(session, transaction),
+      headers: ArcadeHeaders.compose(context.server.headers, transaction),
       body: body ? SpriteBody.compose(body) : null,
       keepalive: true
     });
   }
   /**
    * Make a {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST `POST`} request in which a JSON response is expected.
-   * @param route The route (key) to target in the request.
-   * @param body The {@link https://developer.mozilla.org/en-US/docs/Web/API/Request/body body} to include in the request.
-   * @param session The `DatabaseSession` to target in the request.
-   * @param transaction The `SpriteTransaction` to target in the request.
+   * @param route - The route (key) to target in the request.
+   * @param body - The {@link https://developer.mozilla.org/en-US/docs/Web/API/Request/body body} to include in the request.
+   * @param context.- The `ArcadeClientContext` to target in the request.
+   * @param transaction - The `Transaction` to target in the request.
    * @returns The json response from the ArcadeDB Server.
    */
   public static async postJson<T>(
-    route: ApiRoutes,
+    route: ArcadeRoute,
     body: SpriteRestBody,
-    session: Session,
-    transaction?: SpriteTransaction
+    context: ArcadeClientContext,
+    transaction?: Transaction
   ): Promise<T> {
     return JsonResponse.parse<T>(
-      await this.post(route, body, session, transaction)
+      await this.post(route, body, context, transaction)
     );
   }
 }
