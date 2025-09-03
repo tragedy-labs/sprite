d6307bf7ca0c50507027c7ed4ff48f9cef68b429
Austin Pittman <pittman.austin@gmail.com>
2025-09-03 08:41:12 -0600

    docs: configuring for api-extractor / api-documenter

--- src/api.ts
+++
@@ -1,5 +0,0 @@
-export { SpriteDatabase } from './database/SpriteDatabase.js';
-export { SpriteServer } from './server/SpriteServer.js';
-export { SpriteTransaction } from './transaction/SpriteTransaction.js';
-
-export * from './types/index.js';
