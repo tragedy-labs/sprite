27f4587c9c0a512845d9cb15d50ee51cd0b4083d
Austin Pittman <pittman.austin@gmail.com>
2025-09-04 13:24:41 -0600

    docs: updated documentation to reflect arg changes, and tsdoc liniting reqs

--- src/admin/static/dropUser.ts
+++ src/admin/static/dropUser.ts
@@ -4,8 +4,8 @@ import type { ArcadeAdminContext } from '@/context/ArcadeAdminContext.js';
 
 /**
  * Drop a user from the ArcadeDB server.
- * @param server The server to use to drop the user.
- * @param username The `username` of the user to drop from the ArcadeDB server.
+ * @param context - The context to use to drop the user.
+ * @param username - The `username` property of the user to drop from the ArcadeDB server.
  * @returns `true` if the user was successfully dropped.
  * @throws `Error` if the user could not be dropped.
  */
