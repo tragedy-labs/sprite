c86996f0aa063dfbc50b5a441b4f32fb4f7b482f
Austin Pittman <pittman.austin@gmail.com>
2025-09-04 13:06:25 -0600

    docs: updated documentation to reflect arg changes

--- src/admin/static/booleanAdminCommand.ts
+++ src/admin/static/booleanAdminCommand.ts
@@ -4,10 +4,10 @@ import { ArcadeAdminContext } from '@/context/ArcadeAdminContext.js';
 
 /**
  * Internal method for sending commands to the server in which a JSON response
- * containing an `ok` value in the `result` property is expected. `ok` is then
- * returned as a simple boolean (`true`) value
- * @param adminContext - The server to use to send the command.
- * @param command - The [command](https://docs.arcadedb.com/#HTTP-ServerCommand) to send to the server, such as `CREATE DATABASE`.
+ * containing an `ok` value in the `result` property is expected, which is translated
+ * to a boolean value.
+ * @param context - The context from which to send the command.
+ * @param command - The [command](https://docs.arcadedb.com/#HTTP-ServerCommand) to send to the server (i.e. `CREATE DATABASE`).
  * @returns `true` if the command was successful.
  * @throws `Error` if the command could not be executed.
  */
