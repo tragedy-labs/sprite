afabeb3991f9802ec0659784c502cf87465e6ea2
Austin Pittman <pittman.austin@gmail.com>
2025-09-17 23:39:41 -0600

    fix: renaming

---
+++ src/admin/static/listDatabases.ts
@@ -0,0 +1,24 @@
+import type { ArcadeAdminContext } from '@/context/ArcadeAdminContext.js';
+import { Rest } from '@/rest/Rest.js';
+
+/**
+ * Returns a list of database names that are present on the server.
+ * @param context - The context to use to retrieve the database list from the server.
+ * @returns An array of database names present on the server.
+ * @throws `Error` if the database list could not be retrieved.
+ */
+export async function listDatabases(
+  context: ArcadeAdminContext
+): Promise<Array<string>> {
+  try {
+    return await Rest.getJson(
+      context.endpoints.databases,
+      context.arcade.headers
+    );
+  } catch (error) {
+    throw new Error(
+      'Encountered an error when attemping to fetch list of databases from the server.',
+      { cause: error }
+    );
+  }
+}
